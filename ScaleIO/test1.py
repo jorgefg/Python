61d4768956d7c922128db916308297113db6c3eb
jorgefg <palo@akinosoft.org>
2016-03-06 17:15:49 +0100

    x

--- ScaleIO/test1.py
+++ ScaleIO/test1.py
@@ -10,16 +10,22 @@ from config import *
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 response = requests.get('https://'+sio_host+'/api/login', auth=HTTPBasicAuth(sio_user, sio_pass), verify=False)
-
 token = bytes.decode(base64.b64encode (bytes(':'+json.loads(response.text), "utf-8")))
-
-headers = {'Accept':'application/json', 'Content-Type':'application/json', 'Authorization':'Basic '+token+''}
+headers = {'Content-Type':'application/json', 'Authorization':'Basic '+token+''}
 
 response = requests.get('https://'+sio_host+'/api/types/Volume/instances', headers=headers, verify=False)
-
 data = response.json()
-volume_count = 0
 for volume in data:
-    print (volume['id'],volume['name'],volume['sizeInKb'],volume['volumeType'])
-    volume_count += 1
-
+    response = requests.get('https://'+sio_host+volume['links'][1]['href'], headers=headers, verify=False)
+    statistics = response.json()
+    print (
+        volume['id'],
+        volume['name'],
+        volume['sizeInKb'],
+        volume['volumeType'],
+        volume['useRmcache'],
+        statistics['userDataReadBwc']['totalWeightInKb'],
+        statistics['userDataReadBwc']['numOccured'],
+        statistics['userDataWriteBwc']['totalWeightInKb'],
+        statistics['userDataWriteBwc']['numOccured']
+    )
