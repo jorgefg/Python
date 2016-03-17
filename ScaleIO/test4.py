bea06fa434e4600db6e7b4d61ba2d7a767b9c451
PaLo <palo@akinosoft.org>
2016-03-17 21:00:06 +0100

    .

--- ScaleIO/test4.py
+++ ScaleIO/test4.py
@@ -4,12 +4,16 @@ import uuid
 import json
 import scaleio
 import requests
+import time
 from pprint import pprint
 
 url = 'http://192.168.10.184:9200'
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 for volume in scaleio.get_volumes():
-    data = (json.dumps(scaleio.get_volume(volume['id']),sort_keys=True,indent=4))
-    r = requests.post(url+'/scaleio/volumes/'+str(uuid.uuid4()), data=data, headers=headers)
+    data = json.loads(json.dumps(scaleio.get_volume(volume['id']), sort_keys=True, indent=2))
+    data['date'] = int(time.time())
+    data = json.dumps(data, sort_keys=True, indent=2)
+    r = requests.post(url+'/scaleio/volumes/'+volume['id'], data=data, headers=headers)
+    #r = requests.post(url+'/scaleio/volumes/'+str(uuid.uuid4()), data=data, headers=headers)
     pprint(r.text)
---
+++ ScaleIO/test5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import uuid
+import json
+import scaleio
+import requests
+import time
+from pprint import pprint
+
+url = 'http://192.168.10.184:9200'
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+json_data = {}
+for volume in scaleio.get_volumes():
+    json_data[volume['id']] = volume
+    json_data[volume['id']]['date'] = int(time.time())
+
+r = requests.post(url+'/scaleio/volumes', data=json_data, headers=headers)
+pprint(r.text)
