745454c61ac077b7d8ed09b1fff2cc5b8f840a6c
PaLo <palo@akinosoft.org>
2016-06-15 17:50:33 +0200

    .

--- OpenStack/cinder_scaleio.py
+++ OpenStack/cinder_scaleio.py
@@ -4,7 +4,8 @@ from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from cinderclient.v2 import client
 import base64
-import os
+from prettytable import PrettyTable
+#import os
 
 def get_volume_id(cinder, volume):
     formatted_volume = str(volume)
@@ -13,12 +14,18 @@ def get_volume_id(cinder, volume):
     return id
 
 auth = v3.Password(
-    auth_url=os.environ['OS_AUTH_URL'],
-    username=os.environ['OS_USERNAME'],
-    user_domain_name=os.environ['OS_USER_DOMAIN_NAME'],
-    project_name = os.environ['OS_PROJECT_NAME'],
-    project_domain_name=os.environ['OS_PROJECT_DOMAIN_NAME'],
-    password=os.environ['OS_PASSWORD']
+    #auth_url=os.environ['OS_AUTH_URL'],
+    #username=os.environ['OS_USERNAME'],
+    #user_domain_name=os.environ['OS_USER_DOMAIN_NAME'],
+    #project_name = os.environ['OS_PROJECT_NAME'],
+    #project_domain_name=os.environ['OS_PROJECT_DOMAIN_NAME'],
+    #password=os.environ['OS_PASSWORD']
+    auth_url='http://192.168.10.194:35357/v3',
+    username='admin',
+    user_domain_name='default',
+    project_name='admin',
+    project_domain_name='default',
+    password='changeme'
 )
 
 sess = session.Session(auth=auth,verify=False)
@@ -27,10 +34,12 @@ cinder = client.Client(session=sess,endpoint_type='publicURL')
 
 volumes = cinder.volumes.list()
 
+t = PrettyTable(['OpenStack ID', 'OpenStack Name', 'ScaleIO Name', 'Size (GB)'])
 for volume in volumes:
     volume_id = get_volume_id(cinder, volume)
     volume_name = cinder.volumes.get(volume_id)._info['name']
     volume_size = cinder.volumes.get(volume_id)._info['size']
     volume_sio = volume_id.replace("-", "")
     volume_sio = base64.b64encode(base64.b16decode(volume_sio.upper()))
-    print volume_id,volume_name,volume_sio,volume_size
+    t.add_row([volume_id,volume_name,volume_sio,volume_size])
+print t
