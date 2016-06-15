1712c0d33e039ba5ee288870285e7ddab9bb9417
PaLo <palo@akinosoft.org>
2016-06-15 17:32:37 +0200

    .

--- OpenStack/test_cinder.py
+++ OpenStack/test_cinder.py
@@ -3,18 +3,34 @@
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from cinderclient.v2 import client
+import base64
+import os
+
+def get_volume_id(cinder, volume):
+    formatted_volume = str(volume)
+    splitted_string = formatted_volume.split()
+    id = splitted_string[1].strip('>')
+    return id
 
 auth = v3.Password(
-    auth_url='http://192.168.10.194:5000/v3',
-    username='admin',
-    user_domain_name='Default',
-    project_name = 'admin',
-    project_domain_name='Default',
-    password='changeme'
+    auth_url=os.environ['OS_AUTH_URL'],
+    username=os.environ['OS_USERNAME'],
+    user_domain_name=os.environ['OS_USER_DOMAIN_NAME'],
+    project_name = os.environ['OS_PROJECT_NAME'],
+    project_domain_name=os.environ['OS_PROJECT_DOMAIN_NAME'],
+    password=os.environ['OS_PASSWORD']
 )
 
-sess = session.Session(auth=auth)
+sess = session.Session(auth=auth,verify=False)
 
 cinder = client.Client(session=sess,endpoint_type='publicURL')
 
-cinder.volumes.list()
+volumes = cinder.volumes.list()
+
+for volume in volumes:
+    volume_id = get_volume_id(cinder, volume)
+    volume_name = cinder.volumes.get(volume_id)._info['name']
+    volume_size = cinder.volumes.get(volume_id)._info['size']
+    volume_sio = volume_id.replace("-", "")
+    volume_sio = base64.b64encode(base64.b16decode(volume_sio.upper()))
+    print volume_id,volume_name,volume_sio,volume_size
