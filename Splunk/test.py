79e62d9b7e261957bd39cbe8fa81f7e951f4a3e3
jorgefg <palo@akinosoft.org>
2016-08-08 20:14:06 +0200

    .

--- Splunk/test.py
+++ Splunk/test.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python
 
 import requests
+import json
 
 OS_AUTH_URL='http://192.168.10.194:5000/v3'
+OS_USERNAME='demo'
+OS_PROJECT_DOMAIN_NAME='default'
+OS_PASSWORD='changeme'
 
-class User(object):
-    def __init__(self, name, username):
-        self.name = name
-        self.username = username
+headers = {"Content-type": "application/json"}
 
-data[auth][identity][methods]="password"
-data[auth][identity][password]="password"
+data = {
+    "auth": {
+        "identity": {
+            "methods": ["password"],
+            "password": {
+                "user": {
+                    "name": OS_USERNAME,
+                    "domain": {"id": OS_PROJECT_DOMAIN_NAME},
+                    "password": OS_PASSWORD
+                }
+            }
+        },
+        "scope": {
+            "domain": {
+                "id": OS_PROJECT_DOMAIN_NAME
+            }
+        }
+  }
+}
 
-print data[auth]
-
-#headers = '{'Content-type': 'application/json'}'
+print(json.dumps(data, indent=4, sort_keys=True))
+print(json.dumps(headers, indent=4, sort_keys=True))
 
 #response = requests.get(OS_AUTH_URL+'/auth/tokens', auth=HTTPBasicAuth(sio_user, sio_pass), verify=False)
