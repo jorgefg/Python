19ed29fc2ecfd5b6559ee43f86d4dddbb6deb996
jorgefg <palo@akinosoft.org>
2016-08-10 00:10:58 +0200

    .

--- OpenStack/splunk/servers.py
+++ OpenStack/splunk/servers.py
@@ -10,7 +10,7 @@ response = requests.get(URL + '/servers/detail?all_tenants=1', headers=HEADERS, 
 
 data = json.loads(response.text)
 
-t = [["id", "name", "status", "instance", "zone", "hypervisor", "launched_at", "terminated_at", "updated_at", "user_id", "tenant_id"]]
+t = [["id", "name", "status", "instance", "zone", "hypervisor", "flavor", "launched_at", "terminated_at", "updated_at", "user_id", "tenant_id"]]
 
 for server in data['servers']:
     t.append([
@@ -20,6 +20,7 @@ for server in data['servers']:
         server['OS-EXT-SRV-ATTR:instance_name'],
         server['OS-EXT-AZ:availability_zone'],
         server['OS-EXT-SRV-ATTR:hypervisor_hostname'],
+        server['flavor']['id'],
         server['OS-SRV-USG:launched_at'],
         server['OS-SRV-USG:terminated_at'],
         server['updated'],
@@ -28,4 +29,4 @@ for server in data['servers']:
     ])
 
 for line in t:
-    print("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10}".format(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10]))
+    print("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11}".format(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11]))
