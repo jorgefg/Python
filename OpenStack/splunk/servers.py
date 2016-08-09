33aa0a20f43cc8d0054571a3c1c1ac4c66127893
jorgefg <palo@akinosoft.org>
2016-08-09 21:55:15 +0200

    .

--- OpenStack/splunk/servers.py
+++ OpenStack/splunk/servers.py
@@ -10,26 +10,22 @@ response = requests.get(URL + '/servers/detail?all_tenants=1', headers=HEADERS, 
 
 data = json.loads(response.text)
 
-t = [["id", "name", "status", "type", "zone", "bootable", "cg", "description", "encrypted", "multiattach", "host", "user_id", "tenant_id", "created_at", "updated_at"]]
+t = [["id", "name", "status", "instance", "zone", "hypervisor", "launched_at", "terminated_at", "updated_at", "user_id", "tenant_id"]]
 
 for server in data['servers']:
     t.append([
         server['id'],
         server['name'],
         server['status'],
-        server['volume_type'],
-        server['availability_zone'],
-        server['bootable'],
-        server['consistencygroup_id'],
-        server['description'],
-        server['encrypted'],
-        server['multiattach'],
-        server['os-vol-host-attr:host'],
+        server['OS-EXT-SRV-ATTR:instance_name'],
+        server['OS-EXT-AZ:availability_zone'],
+        server['OS-EXT-SRV-ATTR:hypervisor_hostname'],
+        server['OS-SRV-USG:launched_at'],
+        server['OS-SRV-USG:terminated_at'],
+        server['updated'],
         server['user_id'],
-        server['os-vol-tenant-attr:tenant_id'],
-        server['created_at'],
-        server['updated_at']
+        server['tenant_id']
     ])
 
 for line in t:
-    print("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11};{12};{13};{14}".format(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13], line[14]))
+    print("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10}".format(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10]))
